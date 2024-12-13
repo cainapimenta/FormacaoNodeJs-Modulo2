79700eb2bed3af75c7fc7ecdd9506ed86cd114b2
Cainã Pimenta <caina.pimenta@alfaerp.com.br>
2024-12-13 06:49:24 -0300

    Validação personalizada

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -22,8 +22,14 @@ const livroSchema = new mongoose.Schema({
     preco: { type: Number },
     paginas: {
         type: Number,
-        min: [10, "O número de paginas deve estar entre 10 e 5000. Valor fornecido: {VALUE}"],
-        max: [5000, "O número de paginas deve estar entre 10 e 5000. Valor fornecido: {VALUE}"]
+        validate: {
+            validator: (valor) => {
+                return valor >= 10 && valor <= 5000;
+            },
+            message: "O número de paginas deve estar entre 10 a 5000. Valor fornecido: {VALUE}"
+        }
+        // min: [10, "O número de paginas deve estar entre 10 e 5000. Valor fornecido: {VALUE}"],
+        // max: [5000, "O número de paginas deve estar entre 10 e 5000. Valor fornecido: {VALUE}"]
     }
 
 }, { versionKey: false });
